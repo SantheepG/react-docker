cc9648b79494a2c8daf4095338093b6c0c280f17
SantheepG <gshantheep@gmail.com>
2025-02-14 20:33:29 +0530

    pass test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React App dockerized CICDized
+          I made it
         </a>
       </header>
     </div>

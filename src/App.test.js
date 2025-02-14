2fd136469adf88b1b4a829c762ff50ed2f92e42d
SantheepG <gshantheep@gmail.com>
2025-02-14 20:30:59 +0530

    failed test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/React App dockerized CICDized/i);
+  const linkElement = screen.getByText(/I made it/i);
   expect(linkElement).toBeInTheDocument();
 });

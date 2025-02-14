c6db870d6bc82bf8b834dbeb707d789528902951
SantheepG <gshantheep@gmail.com>
2025-02-14 18:16:52 +0530

    ip

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React App dockerized 20
+          React App dockerized 21
         </a>
       </header>
     </div>

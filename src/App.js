dd14471231ad0e6de508679b5eec831a78e1a5df
SantheepG <gshantheep@gmail.com>
2025-02-14 13:20:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React App dockerized
+          React App dockerized 2
         </a>
       </header>
     </div>

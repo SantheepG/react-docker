0f39150acfe36bbba4890642e0138023a047fbee
SantheepG <gshantheep@gmail.com>
2025-02-14 19:40:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hii
+          React App dockerized CICDized
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Reac/i);
+  const linkElement = screen.getByText(/React App dockerized CICDized/i);
   expect(linkElement).toBeInTheDocument();
 });
